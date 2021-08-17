995aede44c36a9f57abba87659707349dc987975
ade.ristanto@gmail.com <56842597-Ade>
2021-08-17 14:26:38 +0700

    1. Instantiate

---
+++ geometri/persegipanjang.py
@@ -0,0 +1,12 @@
+class PersegiPanjang():
+
+    def __init__(self, p, l):
+        # Fungsi yang dipanggil pertama kali saat object diciptakan
+        self.p = p
+        self.l = l
+
+    def info(self):
+        return f"Ini adalah object dari Persegi panjang dengan panjang = {self.p} dan lebar = {self.l}"
+
+    def hitung_luas(self):
+        return self.p * self.l
---
+++ geometri/segitiga.py
@@ -0,0 +1,11 @@
+class Segitiga():
+
+    def __init__(self, alas, tinggi):
+        self.alas = alas
+        self.tinggi = tinggi
+
+    def info(self):
+        return f"Ini adalah object dari Segitiga dengan alas = {self.alas} dan tinggi = {self.tinggi}"
+
+    def hitung_luas(self):
+        return self.tinggi * (self.alas / 2)
